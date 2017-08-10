9649e6928c5b94fcedc5582e3cfef64f8786a5f7
Juliano Petronetto <juliano.petronetto@picpay.com>
2017-08-10 16:49:17 -0300

    tentativa de testes

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,7 @@
 SECRET_KEY             = 'w46eM4Gyy@BgBtUCt3tM#lIljF$pcAn7pPW0BJ%c1bC73Cgw!Nis4b5kAoTQbQuAi'
 DEBUG                  = True
 MONGODB_DB             = 'flask'
-MONGODB_HOST           = 'mongodb'
+MONGODB_HOST           = '0.0.0.0'
 MONGODB_PORT           = 27017
 WTF_CSRF_CHECK_DEFAULT = False
 WTF_CSRF_ENABLED       = False
