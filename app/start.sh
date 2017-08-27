8f991a68d558f1749e9b2f610b647977181042a9
Juliano Petronetto <juliano.petronetto@gmail.com>
2017-08-27 00:38:26 -0300

    Improve the start.sh and add gevent

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
-procs=$(cat /proc/cpuinfo | grep processor | wc -l)
+# (2 * cores) + 1
+workers=$(($((2 * ($(cat /proc/cpuinfo | grep processor | wc -l)))) + 1))
 
-gunicorn -w $procs -b 0.0.0.0:5000 app.wsgi:app
+if [[ $PORT ]]; then
+    gunicorn -w $workers -k gevent -b 0.0.0.0:$PORT app.wsgi:app
+else
+    gunicorn -w $workers -k gevent -b 0.0.0.0:5000 app.wsgi:app
+fi
+
